63c968b8424280da1202250b2a411f115c382c14
MadihaJarrin <madihajarrin1@gmail.com>
2022-01-18 03:10:12 +0600

    added

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
 import Services from '../Services/Services';
+import education from '../../../images/management.png'
 
 const Home = () => {
     return (
--- src/Components/Home/Services/Services.js
+++ src/Components/Home/Services/Services.js
@@ -5,23 +5,32 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Container } from '@mui/material';
 import Service from '../Service/Service';
+import education from '../../../images/education.png';
+import professional from '../../../images/professional.png';
+import research from '../../../images/research.png';
+import management from '../../../images/management.png';
+
 
 const services = [
     {
         name: 'Education',
-        description: 'Education is the backbone of a nation'
+        description: 'Education is the backbone of a nation',
+        img: education
     },
     {
         name: 'Professional',
-        description: 'Education is the backbone of a nation'
+        description: 'Education is the backbone of a nation',
+        img: professional
     },
     {
         name: 'Research',
-        description: 'Education is the backbone of a nation'
+        description: 'Education is the backbone of a nation',
+        img: research
     },
     {
         name: 'Management',
-        description: 'Education is the backbone of a nation'
+        description: 'Education is the backbone of a nation',
+        img: management
     }
 
 ]
@@ -36,15 +45,17 @@ const Item = styled(Paper)(({ theme }) => ({
 const Services = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
+
             <Container>
 
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
 
-                    {services.map((_, index) => (
-                        <Grid item xs={4} sm={4} md={3} key={index}>
-                            <Service></Service>
-                        </Grid>
-                    ))}
+                    {
+                        services.map(service => <Service
+                            key={service.name}
+                            service={service}
+                        ></Service>)
+                    }
                 </Grid>
 
             </Container>
